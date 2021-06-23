9238a5ea746cc55dc387fcc025691e3ccb3e5bd4
apcavalheiro <adrianocavalheiro1@gmail.com>
2021-06-23 16:13:12 -0300

    ajustes docker-compose

---
+++ mural_api/src/test/java/br/edu/ifrs/restinga/grupo_1/mural_api/MuralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.grupo_1.mural_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
