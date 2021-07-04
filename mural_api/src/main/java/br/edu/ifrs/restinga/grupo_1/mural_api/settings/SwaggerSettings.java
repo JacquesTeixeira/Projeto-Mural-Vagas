ed4db343fc8c791a5683f4eefe28db7a51b80ff7
apcavalheiro <adrianocavalheiro1@gmail.com>
2021-07-04 01:39:13 -0300

    Add Swagger

--- mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/settings/SwaggerSettings.java
+++ mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/settings/SwaggerSettings.java
@@ -1,2 +1,35 @@
-package br.edu.ifrs.restinga.grupo_1.mural_api.settings;public class SwaggerSettings {
+package br.edu.ifrs.restinga.grupo_1.mural_api.settings;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerSettings {
+
+
+    @Bean
+    public Docket apiV100() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("br.edu.ifrs.restinga.grupo_1.mural_api"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("API MURAL DE VAGAS IFRS - RESTINGA")
+                .description(
+                        "API Mural de vagas\n" +
+                        "Trabalho disciplina DEVII 2021\n" +
+                        "GRUPO1: Adriano, Guilherme, Jacques, Tiago e Vitor. ")
+                .version("1.0.0")
+                .build();
+    }
 }
