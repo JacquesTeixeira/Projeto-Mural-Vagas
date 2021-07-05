a96424c8bece443b3ab35ec5ea78ced90a007aad
apcavalheiro <adrianocavalheiro1@gmail.com>
2021-07-04 23:43:14 -0300

    Ajustes modelo vaga, add metodos a serem implementados na interface emailservice

--- mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/EmailService.java
+++ mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.edu.ifrs.restinga.grupo_1.mural_api.services;
 
+import br.edu.ifrs.restinga.grupo_1.mural_api.models.Candidato;
+import br.edu.ifrs.restinga.grupo_1.mural_api.models.Vaga;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
@@ -8,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class EmailService extends EmailServiceImpl {
+public class EmailService extends EmailServiceImpl{
 
     @Autowired
     private MailSender mailSender;
@@ -16,7 +18,22 @@ public class EmailService extends EmailServiceImpl {
     @Override
     public void enviarEmail(SimpleMailMessage msg) {
         log.info("Enviando email...");
-        mailSender.send(msg);
+        this.mailSender.send(msg);
         log.info("Email enviado");
+    }
+
+    @Override
+    public void notificacaConfirmarCadastro(Candidato candidato) {
+
+    }
+
+    @Override
+    public void notificacaoVagaAtualizada(Vaga vaga) {
+
+    }
+
+    @Override
+    public void notificacaoVagaNova(Candidato candidato, Vaga vaga) {
+
     }
 }
--- mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/EmailServiceImpl.java
+++ mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/EmailServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.Date;
 
 public abstract class EmailServiceImpl implements EmailServiceInterface {
 
-    @Override
     public void confirmacaoCadastro(Candidato obj) {
         SimpleMailMessage sm = this.prepareConfirmacaoCadastro(obj);
         enviarEmail(sm);
@@ -16,9 +15,9 @@ public abstract class EmailServiceImpl implements EmailServiceInterface {
     protected SimpleMailMessage prepareConfirmacaoCadastro(Candidato obj) {
         SimpleMailMessage sm = new SimpleMailMessage();
         sm.setTo(obj.getEmail());
-        sm.setSubject("Olá, " + obj.getNome() + " cadastro realizado comsucesso!");
+        sm.setSubject("Seu cadastro foi realizado com sucesso!");
         sm.setSentDate(new Date(System.currentTimeMillis()));
-        sm.setText("Cadastre agora seu portfólio" +
+        sm.setText("Olá, " + obj.getNome() + " cadastre agora seu portfólio " +
                 "para receber informações sobre as vagas que melhor se enquadram com seu perfil!");
         return sm;
     }
