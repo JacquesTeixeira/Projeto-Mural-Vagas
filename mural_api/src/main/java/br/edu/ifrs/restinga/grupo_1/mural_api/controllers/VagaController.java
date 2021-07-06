d63a1f0cdd74093f8678df6de2ced4c97b1d8eaf
Tiago Figueira <tiago.g.figueira@gmail.com>
2021-07-06 01:21:35 -0300

    Inserção dos crud's restantes e ajuste no service

--- mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/controllers/VagaController.java
+++ mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/controllers/VagaController.java
@@ -1,17 +1,16 @@
 package br.edu.ifrs.restinga.grupo_1.mural_api.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.edu.ifrs.restinga.grupo_1.mural_api.models.Vaga;
 import br.edu.ifrs.restinga.grupo_1.mural_api.services.VagaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/vagas")
@@ -21,19 +20,44 @@ public class VagaController {
 	private VagaService vagaService;
 	
 	@RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<Vaga>> buscarTodas() {
-        List<Vaga> vagas = vagaService.buscarTodas();
+    public ResponseEntity<List<Vaga>> all() {
+        List<Vaga> vagas = this.vagaService.buscarTodas();
         return ResponseEntity.ok().body(vagas);
     }
 
     @RequestMapping(value="/paginado", method=RequestMethod.GET)
-    public ResponseEntity<Page<Vaga>> buscarPorPagina(
+    public ResponseEntity<Page<Vaga>> paginate(
             @RequestParam(value="pagina", defaultValue="0") Integer pagina,
             @RequestParam(value="linhasPorPagina", defaultValue="24") Integer linhasPorPagina,
             @RequestParam(value="ordem", defaultValue="nome") String ordem,
             @RequestParam(value="direcao", defaultValue="ASC") String direcao) {
-        Page<Vaga> vagas = vagaService.buscarPaginado(pagina, linhasPorPagina, ordem, direcao);
+        Page<Vaga> vagas = this.vagaService.buscarPaginado(pagina, linhasPorPagina, ordem, direcao);
         return ResponseEntity.ok().body(vagas);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Vaga> show(@PathVariable("id") Long id) {
+	    return ResponseEntity.ok().body(this.vagaService.buscarPorId(id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> create(@RequestBody Vaga vaga) {
+	    Vaga obj = this.vagaService.cadastrar(vaga);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Vaga vaga, @PathVariable Long id) {
+        this.vagaService.editar(vaga, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> destroy(@PathVariable Long id) {
+	    this.vagaService.excluir(id);
+	    return ResponseEntity.noContent().build();
     }
 	
 	
--- mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/VagaService.java
+++ mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/VagaService.java
@@ -2,7 +2,10 @@ package br.edu.ifrs.restinga.grupo_1.mural_api.services;
 
 
 
+import br.edu.ifrs.restinga.grupo_1.mural_api.services.exceptions.DataIntegrityException;
+import br.edu.ifrs.restinga.grupo_1.mural_api.services.exceptions.ObjectNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
@@ -10,8 +13,10 @@ import org.springframework.stereotype.Service;
 
 import br.edu.ifrs.restinga.grupo_1.mural_api.models.Vaga;
 import br.edu.ifrs.restinga.grupo_1.mural_api.repositories.VagaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class VagaService {
@@ -20,11 +25,68 @@ public class VagaService {
 	private VagaRepository vagaRepository;
 	
 	public List<Vaga> buscarTodas() {
-		return vagaRepository.findAll();
+		List<Vaga> vagas = this.vagaRepository.findAll();
+		if(vagas.isEmpty()) {
+			throw new ObjectNotFound("Base de dados vazia!");
+		}
+		return vagas;
 	}
-	
+
+
+    public Vaga buscarPorId(Long id) {
+		try {
+			return this.vagaRepository.findById(id).orElseThrow(()-> new ObjectNotFound("Não existe vaga com o id informado!"));
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFound("Não existe vaga com o id informado!");
+		}
+	}
+
+	@Transactional
+	public Vaga cadastrar(Vaga vaga) {
+		try {
+			vaga = this.vagaRepository.save(vaga);
+		} catch (Exception e) {
+			throw new DataIntegrityException("Não foi possível cadastrar, verifique os dados informados!");
+		}
+		return vaga;
+	}
+
+	@Transactional
+	public Vaga editar(Vaga vaga, Long id) {
+		try {
+			Vaga vagaDb = this.buscarPorId(id);
+			vagaDb.setTitulo(vaga.getTitulo());
+			vagaDb.setDescricao(vaga.getDescricao());
+			vagaDb.setArea(vaga.getArea());
+			vagaDb.setEmpresa(vaga.getEmpresa());
+			vagaDb.setRequisitos(vaga.getRequisitos());
+			vagaDb.setDesejavel(vaga.getDesejavel());
+			vagaDb.setDiferenciais(vaga.getDiferenciais());
+			vagaDb.setSalario(vaga.getSalario());
+			vagaDb.setBeneficios(vaga.getBeneficios());
+			return vagaDb;
+		} catch (Exception e) {
+			throw new DataIntegrityException("Não foi possível atualizar, verifique os dados informados!");
+		}
+	}
+
+	public void excluir(Long id) {
+		this.buscarPorId(id);
+		try {
+			this.vagaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir a vaga!");
+		}
+	}
+
 	public Page<Vaga> buscarPaginado(Integer pagina, Integer linhasPorPagina, String ordem, String direcao) {
-        PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direcao), ordem);
-        return vagaRepository.findAll(pageRequest);
-    }
+		PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direcao), ordem);
+		Page<Vaga> vagas = this.vagaRepository.findAll(pageRequest);
+		if(vagas.isEmpty()) {
+			throw new ObjectNotFound("Base de dados vazia!");
+		}
+		return vagas;
+	}
+
+
 }
