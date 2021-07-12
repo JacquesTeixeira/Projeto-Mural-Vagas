8fbe00328ed341bb44dae4ee8b45ce0043a4cd79
JacquesTeixeira <jmteixeira@restinga.ifrgs.edu.br>
2021-07-11 22:24:22 -0300

    Criacao de servico e repositorio

--- mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/VagaService.java
+++ mural_api/src/main/java/br/edu/ifrs/restinga/grupo_1/mural_api/services/VagaService.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.edu.ifrs.restinga.grupo_1.mural_api.models.AreaDaVaga;
 import br.edu.ifrs.restinga.grupo_1.mural_api.models.Vaga;
+import br.edu.ifrs.restinga.grupo_1.mural_api.repositories.AreaDaVagaRepository;
 import br.edu.ifrs.restinga.grupo_1.mural_api.repositories.VagaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +25,9 @@ public class VagaService {
 	
 	@Autowired
 	private VagaRepository vagaRepository;
+	
+	@Autowired
+    private AreaDaVagaRepository areaDaVagaRepository;
 	
 	public List<Vaga> buscarTodas() {
 		List<Vaga> vagas = this.vagaRepository.findAll();
@@ -93,6 +98,25 @@ public class VagaService {
 		return vagas;
 
 	}
+	
+	public AreaDaVaga buscarAreaDaVaga(Long id) {
+        try {
+            return this.areaDaVagaRepository.findById(id).orElseThrow(() -> new ObjectNotFound("Não existe área de vaga com o id informado"));
+        } catch (NoSuchElementException e) {
+            throw new ObjectNotFound("Não existe área de vaga com o id informado");
+        }
+    }
 
-
+	@Transactional
+    public AreaDaVaga cadastrarAreaDaVaga(AreaDaVaga areaDaVaga, Long vagaId) {
+        Vaga vagaDb = this.buscarPorId(vagaId);
+        try {
+            AreaDaVaga areaDaVagaSalvo = this.areaDaVagaRepository.save(areaDaVaga);
+            vagaDb.setAreaDaVaga(areaDaVagaSalvo);
+            this.vagaRepository.save(vagaDb);
+        } catch (Exception e) {
+            throw new DataIntegrityException("Não foi possível cadastar a área da vaga, verifique os dados informados!");
+        }
+        return areaDaVaga;
+    }
 }
